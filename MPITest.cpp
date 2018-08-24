6a575d9d360cb19f94979b6fe666a48ca1a729dd
Di.Liu <38807711+Nakeymonkey@users.noreply.github.com>
2018-08-24 20:13:29 +0100

    Update MPITest.cpp

--- MPITest.cpp
+++ MPITest.cpp
@@ -7,23 +7,25 @@
 #include"mpi.h"
 using namespace std;
 vector<float> arr;
-
-float compute_avg(float *array, int tasks) {
-	float sum= 0.000000f;
-	int i;
-	for (i = 0; i < tasks; i++) {
-		sum += array[i];
+/*comput the maximum number*/
+float compute_max(float *array, int tasks) {
+	float max1=0;
+	for (int i = 0; i < tasks; i++) {
+	if (array[i] > max1) {
+		max1 = array[i];
 	}
-	return sum / tasks;
+}
+	return max1;
 }
 
 int main(int argc, char *argv[])
 {
-	/*initial parameter*/
+	/*initial processor*/
 	MPI_Init(&argc, &argv);
 	int numtasks, taskid,  chunksize;	
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+	/*Read file*/
 	std::ifstream infile("Glioma.arrays.txt");	
 	int w=0;
 	if (infile.is_open()) {
@@ -45,17 +47,16 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	
-	
+	infile.close();
+	/*convert vector arr into float *arr */
 	float *arr1 = (float *)malloc(sizeof(float) * w);
 	assert(arr1 != NULL);
 	for (int i = 0; i < w; i++) {
 		arr1[i] = arr[i];
 	}
-
-		infile.close();
-
 	     chunksize = w / numtasks;
+
+	/*Scatter the datas into processors*/
 		 float *Rdata = (float *)malloc(sizeof(float) * chunksize);
 		 assert(Rdata != NULL);
 		  MPI_Scatter( arr1, chunksize, MPI_FLOAT, Rdata,
@@ -74,16 +75,15 @@ int main(int argc, char *argv[])
 				  f++;
 			  }
 		  }
-
+/*get G2, G2 oligo, G3, G3 oligo, G4,non-tumour's maximum value in different blocks */
 		  float G2score=Bar[0][0];
-		  for (int t = 0; t < chunksize/bars; t++) {
+		  for (int t = 0; t < chunksize / bars; t++) {
 			  for (int n = 0; n < 7; n++) {
 				  if (Bar[n][t] > G2score) {
 					  G2score = Bar[n][t];
 				  }
 			  }
 		  }
-		  
 		  cout <<  G2score << "\t";
 		  float G2oligoscore = Bar[7][0];
 		  for (int t = 0; t < chunksize / bars; t++) {
@@ -135,67 +135,69 @@ int main(int argc, char *argv[])
 		  }
 		  cout << nontumourscore << "\n";
 
-		  float *G2score_avgs = (float *)malloc(sizeof(float) * numtasks);
-		  assert(G2score_avgs != NULL);
+/*Pick several significant nodes*/
 
-		  MPI_Gather(&G2score, 1, MPI_FLOAT, G2score_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-		  float score1 = compute_avg(G2score_avgs, numtasks);
+		  float *G2score_max = (float *)malloc(sizeof(float) * numtasks);
+		  assert(G2score_max != NULL);
+
+		  MPI_Gather(&G2score, 1, MPI_FLOAT, G2score_max, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		  float score1 = compute_max(G2score_max, numtasks);
 
 		  cout <<" G2score "<< score1 << "\n";
 
 
 
-		  float *G2oligoscore_avgs = (float *)malloc(sizeof(float) * numtasks);
-		  assert(G2oligoscore_avgs != NULL);
+		  float *G2oligoscore_max = (float *)malloc(sizeof(float) * numtasks);
+		  assert(G2oligoscore_max != NULL);
 
-		  MPI_Gather(&G2oligoscore, 1, MPI_FLOAT, G2oligoscore_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-		  float score2 = compute_avg(G2oligoscore_avgs, numtasks);
+		  MPI_Gather(&G2oligoscore, 1, MPI_FLOAT, G2oligoscore_max, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		  float score2 = compute_max(G2oligoscore_max, numtasks);
 
 		  cout << " G2oligoscore " << score2 << "\n";
 
 
 
-		  float *G3score_avgs = (float *)malloc(sizeof(float) * numtasks);
-		  assert(G3score_avgs != NULL);
+		  float *G3score_max = (float *)malloc(sizeof(float) * numtasks);
+		  assert(G3score_max != NULL);
 
-		  MPI_Gather(&G3score, 1, MPI_FLOAT, G3score_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-		  float score3 = compute_avg(G3score_avgs, numtasks);
+		  MPI_Gather(&G3score, 1, MPI_FLOAT, G3score_max, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		  float score3 = compute_max(G3score_max, numtasks);
 
 		  cout <<  " G3score " << score3 << "\n";
 
 
 
-		  float *G3oligoscore_avgs = (float *)malloc(sizeof(float) * numtasks);
-		  assert(G3oligoscore_avgs != NULL);
+		  float *G3oligoscore_max = (float *)malloc(sizeof(float) * numtasks);
+		  assert(G3oligoscore_max != NULL);
 
-		  MPI_Gather(&G3oligoscore, 1, MPI_FLOAT, G3oligoscore_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-		  float score4 = compute_avg(G3oligoscore_avgs, numtasks);
+		  MPI_Gather(&G3oligoscore, 1, MPI_FLOAT, G3oligoscore_max, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		  float score4 = compute_max(G3oligoscore_max, numtasks);
 
 		  cout << " G3oligoscore " << score4 << "\n";
 
 
-		  float *G4score_avgs = (float *)malloc(sizeof(float) * numtasks);
-		  assert(G4score_avgs != NULL);
+		  float *G4score_max = (float *)malloc(sizeof(float) * numtasks);
+		  assert(G4score_max != NULL);
 
-		  MPI_Gather(&G4score, 1, MPI_FLOAT, G4score_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-		  float score5 = compute_avg(G4score_avgs, numtasks);
+		  MPI_Gather(&G4score, 1, MPI_FLOAT, G4score_max, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		  float score5 = compute_max(G4score_max, numtasks);
 
 		  cout << " G4score " << score5 << "\n";
 
 
 
-		  float *nontumourscore_avgs = (float *)malloc(sizeof(float) * numtasks);
-		  assert(nontumourscore_avgs != NULL);
+		  float *nontumourscore_max = (float *)malloc(sizeof(float) * numtasks);
+		  assert(nontumourscore_max != NULL);
 
-		  MPI_Gather(&nontumourscore, 1, MPI_FLOAT, nontumourscore_avgs, 1, MPI_FLOAT,0, MPI_COMM_WORLD);
-		  float score = compute_avg(nontumourscore_avgs, numtasks);
+		  MPI_Gather(&nontumourscore, 1, MPI_FLOAT, nontumourscore_max, 1, MPI_FLOAT,0, MPI_COMM_WORLD);
+		  float score = compute_max(nontumourscore_max, numtasks);
 		
 		  cout << " nontumourscore " << score << "\n";
 		 
 
 		  cin.get();
 		  cin.get();
-
+		  /*clean arrays*/
 		  free(Rdata);
 		  free(Bar);
 

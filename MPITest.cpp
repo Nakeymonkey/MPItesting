231df41da83192a589369e30249a9a878887c072
Di.Liu <38807711+Nakeymonkey@users.noreply.github.com>
2018-09-17 13:43:29 +0100

    Update MPITest.cpp
    change logic, generate correlation coeeficient

--- MPITest.cpp
+++ MPITest.cpp
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <vector>
 #include"mpi.h"
+#include <math.h>
+
 #define bars 176
 #define numofcluster 6
 using namespace std;
@@ -12,7 +14,8 @@ vector<float> arr;
 /*initialize environment*/
 float *initial(string textname);
 /*comput the average number*/
-void rowavgscore(int start, int end, int platform,int rows);
+void rowavgscore(int start, int end, int platform, int rows);
+float correlationCoefficient(float X[], float Y[]);
 /*initialize data*/
 void dataInitial();
 int chunksize;
@@ -22,6 +25,11 @@ float *significantnodes = new float[numofcluster*numtasks];
 float Rdata[9611008];
 float** Bar = new float*[bars];
 float** Rev = new float*[numofcluster];
+
+
+
+
+
 int main(int argc, char *argv[])
 {
 	float sum = 0;
@@ -29,36 +37,43 @@ int main(int argc, char *argv[])
 	float *Arr = initial("Glioma.arrays.txt");
 
 
-	MPI_Scatter( Arr, chunksize, MPI_FLOAT, Rdata,
-			  chunksize, MPI_FLOAT, 0, MPI_COMM_WORLD);	
+	MPI_Scatter(Arr, chunksize, MPI_FLOAT, Rdata,
+		chunksize, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
 	dataInitial();
-
 	
-	for (int i = 0; i < chunksize / bars; i++) {
-		for (int n = 0; n < numofcluster; n++) {
-			cout << Rev[n][i] << "\t";
-		}
+	ofstream myfile;
+	myfile.open("correlationCoefficient.txt");
+	
+	for (int i = 0; i <chunksize / bars; i++) {
+		float X[] = {Rev[0][i],Rev[1][i], Rev[2][i], Rev[3][i],  Rev[4][i], Rev[5][i] };
+		for (int n = i; n <chunksize / bars; n++) {
+
+			float Y[] = { Rev[0][n+1], Rev[1][n + 1], Rev[2][n + 1],  Rev[3][n + 1], Rev[4][n + 1], Rev[5][n+1] };
+			myfile << correlationCoefficient(X, Y) << "\t";			
+		}	
 		cout << "\n";
-	}
-
-		 MPI_Gather(significantvalue, numofcluster, MPI_FLOAT, significantnodes, numofcluster, MPI_FLOAT, 0, MPI_COMM_WORLD);
 		
-		  cin.get();
-		  cin.get();
-		  /*clean arrays*/
-		  free(Rdata);
+	}
+	myfile.close();
 	
+	MPI_Gather(significantvalue, numofcluster, MPI_FLOAT, significantnodes, numofcluster, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+	cin.get();
+	cin.get();
+	/*clean arrays*/
+	free(Rdata);
+
 	MPI_Finalize();
 	/*for (int i = 0; i < numofcluster*numtasks; i++) {
-		cout << significantnodes[i] << "\t";
+	cout << significantnodes[i] << "\t";
 	}
 	*/
 	return 0;
 }
 
 float *initial(string textname) {
-	
+
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
 	std::ifstream infile(textname);
@@ -72,13 +87,12 @@ float *initial(string textname) {
 				arr.push_back(word);
 				w++;
 			}
-			
+
 		}
-		
+
 	}
 	infile.close();
-	//cout << arr[1];
-	/*convert vector arr into float *arr */
+
 	float *arr1 = new float[w];
 	for (int i = 0; i < w; i++) {
 		arr1[i] = arr[i];
@@ -87,13 +101,13 @@ float *initial(string textname) {
 	return arr1;
 }
 
-void rowavgscore( int start, int end, int platform,int rows) {
-	float sum = 0;	
-		for (int t = start; t < end; t++) {
-			sum = sum + Bar[t][rows];
-		}
-		Rev[platform][rows] = sum / (end-start);
-	
+void rowavgscore(int start, int end, int platform, int rows) {
+	float sum = 0;
+	for (int t = start; t < end; t++) {
+		sum = sum + Bar[t][rows];
+	}
+	Rev[platform][rows] = sum / (end - start);
+
 }
 void dataInitial() {
 	int f = 0;
@@ -113,13 +127,41 @@ void dataInitial() {
 		Rev[n] = new float[chunksize / bars];
 	}
 	for (int n = 0; n < chunksize / bars; n++) {
-		for (int i = 0; i < numofcluster; i++) {
-			rowavgscore(0, 7, i, n);
-			rowavgscore(7, 45, i, n);
-			rowavgscore(45, 64, i, n);
-			rowavgscore(64, 76, i, n);
-			rowavgscore(76, 153, i, n);
-			rowavgscore(153, 176, i, n);
-		}
+		
+			rowavgscore(0, 7, 0, n);
+			rowavgscore(7, 45, 1, n);
+			rowavgscore(45, 64, 2, n);
+			rowavgscore(64, 76, 3, n);
+			rowavgscore(76, 153, 4, n);
+			rowavgscore(153, 176, 5, n);
+		
 	}
+}
+
+float correlationCoefficient(float X[], float Y[])
+{
+
+	float sum_X = 0, sum_Y = 0, sum_XY = 0;
+	float squareSum_X = 0, squareSum_Y = 0;
+
+	for (int i = 0; i < numofcluster; i++)
+	{
+		// sum of elements of array X.
+		sum_X = sum_X + X[i];
+
+		// sum of elements of array Y.
+		sum_Y = sum_Y + Y[i];
+
+		// sum of X[i] * Y[i].
+		sum_XY = sum_XY + X[i] * Y[i];
+
+		// sum of square of array elements.
+		squareSum_X = squareSum_X + X[i] * X[i];
+		squareSum_Y = squareSum_Y + Y[i] * Y[i];
+	}
+	
+
+	float corr = (numofcluster * sum_XY - sum_X * sum_Y)/ sqrt((numofcluster * squareSum_X - sum_X * sum_X)*(numofcluster * squareSum_Y - sum_Y * sum_Y));
+
+	return corr;
 }
